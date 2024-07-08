923dc32504df27439fe4fedb3547cf49fd68abfc
Konstanty Kowalewski <konstanty@kszk.eu>
2024-07-08 17:34:48 +0100

    feat(picom): force average brightness

--- modules/picom.nix
+++ modules/picom.nix
@@ -1,6 +1,21 @@
+/*
+
+Xorg allows an external process
+to manage how off-screen buffers
+are merged with on-screen buffers
+e.g. fade or blur window tooltips.
+DEs often have compositors, WMs not.
+Picom is a compositor for Xorg.
+
+*/
+
 {
   services.picom = {
     enable = true;
     vSync = true;
+    settings = {
+      max-brightness = 1.0;
+      use-damage = false;
+    };
   };
 }
