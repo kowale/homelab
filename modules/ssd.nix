d70b869c46169ecf1442c643fc681856352f8b54
Konstanty Kowalewski <konstanty@kszk.eu>
2024-07-08 17:34:34 +0100

    fix(ssd): remove scrutiny (for now)

--- modules/ssd.nix
+++ modules/ssd.nix
@@ -1,3 +1,13 @@
 {
   services.fstrim.enable = true;
+
+  # services.scrutiny = {
+  #   enable = true;
+  #   collector.enable = true;
+  #   openFirewall = true;
+  #   settings = {
+  #     web.listen.port = 9584;
+  #   };
+  # };
+
 }
