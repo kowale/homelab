e2827db1d8c2af18e01b09c3edca708a7efafb9a
Konstanty Kowalewski <konstanty@kszk.eu>
2024-07-11 17:30:05 +0100

    chore(secrets): rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -8,9 +8,8 @@ in
 
   {
     "hello.age".publicKeys = everyone;
+    "kon.age".publicKeys = everyone;
     "wireless.age".publicKeys = everyone;
     "homelab.kdbx.tar.age".publicKeys = everyone;
-
-    # cache.pear.local:NdBzAs/wPQnM5PYbpwtyA32z+eDpQ+czQKO+IwvTbkQ=
-    "binary_cache_key.age".publicKeys = pear ++ kon;
+    "binary_cache_key.age".publicKeys = pear ++ kon; # cache.pear.local:NdBzAs/wPQnM5PYbpwtyA32z+eDpQ+czQKO+IwvTbkQ=
   }
