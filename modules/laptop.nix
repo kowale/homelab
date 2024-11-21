fdd15ea2f668a788ba672d32f4b0e266b0859eb0
Konstanty Kowalewski <konstanty@kszk.eu>
2024-11-21 18:36:37 +0000

    fix: reduce perf on battery

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -5,21 +5,24 @@
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      # CPU_BOOST_ON_BAT = 0;
-      # RUNTIME_PM_ON_BAT = "auto";
-      # CPU_MIN_PERF_ON_AC = 0;
-      # CPU_MAX_PERF_ON_AC = 100;
-      # CPU_MIN_PERF_ON_BAT = 0;
-      # CPU_MAX_PERF_ON_BAT = 100;
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 70;
+
       START_CHARGE_THRESH_BAT0 = 40;
       STOP_CHARGE_THRESH_BAT0 = 80;
+
+      # CPU_BOOST_ON_BAT = 0;
+      # RUNTIME_PM_ON_BAT = "auto";
     };
   };
 
-
-  security.protectKernelImage = false;
+  # security.protectKernelImage = false;
 
   # services.upower.enable = true;
 
@@ -28,10 +31,10 @@
   #   powertop.enable = true;
   # };
 
-  services.thermald = {
-    enable = true;
-    ignoreCpuidCheck = true; # --ignore-cpuid-check
-  };
+  # services.thermald = {
+  #   enable = true;
+  #   ignoreCpuidCheck = true; # --ignore-cpuid-check
+  # };
 
   # TODO: seems to cause freeze sometimes, to be investigated
   # Disable wakeups (interrupts, disturabnces) on first 4 cores
